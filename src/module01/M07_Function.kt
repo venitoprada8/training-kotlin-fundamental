0c50719b99025b760c529a8632fa1839a572aa59
kelvin373-ht <kelvinht373@gmail.com>
2020-01-18 21:21:55 +0700

    Function

---
+++ src/module01/M07_Function.kt
@@ -0,0 +1,16 @@
+package module01
+
+fun main() {
+    // TODO: Function Basic
+    function01()
+    val message = function02()
+    println(message)
+}
+
+fun function01() {
+    println("Hello Kelvin, Kelvin very happy learn Kotlin Programming")
+}
+
+fun function02(): String {
+    return "You are the best"
+}
