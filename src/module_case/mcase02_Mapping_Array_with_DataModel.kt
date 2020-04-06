eecf3688177ea2a35709dac984f74da78d73198d
kelvin373-ht <kelvinht373@gmail.com>
2020-04-06 19:14:22 +0700

    Case 02: Mapping Array with Data Model

---
+++ src/module_case/mcase02_Mapping_Array_with_DataModel.kt
@@ -0,0 +1,57 @@
+package module_case
+
+data class Dummy(val group: String, val memberGroup: String, val statusGroup: Boolean)
+
+fun getDummy(): ArrayList<Dummy> {
+    val dataDummy = ArrayList<Dummy>()
+
+    dataDummy.add(Dummy("Programming Language", "Java", true))
+    dataDummy.add(Dummy("Programming Language", "Kotlin", true))
+    dataDummy.add(Dummy("Programming Language", "Apple", false))
+    dataDummy.add(Dummy("Programming Language", "MacBook", false))
+    dataDummy.add(Dummy("Programming Language", "Swift", true))
+
+    dataDummy.add(Dummy("Fruits", "Java", false))
+    dataDummy.add(Dummy("Fruits", "Kotlin", false))
+    dataDummy.add(Dummy("Fruits", "Apple", true))
+
+    dataDummy.add(Dummy("Laptop", "Java", false))
+    dataDummy.add(Dummy("Laptop", "Kotlin", false))
+
+    dataDummy.add(Dummy("Animal", "Cow", true))
+    dataDummy.add(Dummy("Animal", "Mango", false))
+    dataDummy.add(Dummy("Animal", "Dog", true))
+    dataDummy.add(Dummy("Animal", "Cat", true))
+
+    return dataDummy
+}
+
+fun main() {
+    val dataDummy = getDummy()
+    val mapList = HashMap<String, List<Dummy>>()
+    var title = dataDummy[0].group
+    var xChildStart = 0
+    var xChildFinish = 0
+    var x = 0
+    dataDummy.forEach {
+        if (title != it.group) {
+            if (x == 0) {
+                title = it.group
+            }
+            else {
+                mapList[title] = (xChildStart until xChildFinish).map { i -> dataDummy[i] }
+                title = it.group
+                xChildStart = xChildFinish
+            }
+        }
+        else if (x == dataDummy.size-1) mapList[title] = (xChildStart..xChildFinish).map { i -> dataDummy[i] }
+        xChildFinish++
+        x++
+    }
+
+    val keyList = ArrayList(mapList.keys)
+    val valueList = ArrayList(mapList.values)
+    for (i in 0 until keyList.size) {
+        println("${keyList[i]} <-> ${valueList[i]}")
+    }
+}
